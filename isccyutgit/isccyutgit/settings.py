e1768c7551ff7d3305c96a69f2935a096b815311
isccyut <isccyut@isccyut-M11BB>
2019-06-13 20:00:50 +0800

    5161

--- isccyutgit/isccyutgit/settings.py
+++ isccyutgit/isccyutgit/settings.py
@@ -76,27 +76,7 @@
 # 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-# 
-# 
-# # Password validation
-# # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
-# 
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+
 #     },
 # ]
 # 
